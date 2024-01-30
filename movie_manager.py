0fe147dbc9974ddadd67313c7211ca338c87988c
Vili Mujkanovic <h23vilmu@du.se>
2024-01-30 14:37:04 +0100

    added menu

---
+++ movie_manager.py
@@ -0,0 +1,3 @@
+import requests as r
+
+#https://www.omdbapi.com/?apikey=5d7a7e3e&s=searched_movie
---
+++ user_menu.py
@@ -0,0 +1,30 @@
+import requests as r
+ # https://www.omdbapi.com/?apikey=5d7a7e3e&s=searched_movie
+def menu_options():
+    menu = """
+[1] Search movies based on it's exact title.
+[2] Search movies based on a word in the title.
+[3] Show history of previously searched movies.
+[4] Exit program.
+"""
+    print(menu)
+def menu_selection(select):
+    if select == 1:
+        print("You have selected 1")
+    elif select == 2:
+        print("You have selected 2")
+    elif select == 3:
+        print("You have selected 3")
+    elif select == 4:
+        print("Exiting...")
+        exit()
+    else:
+        print("Invalid selection. Please try again.")
+def menu_choice():
+    while True:
+        menu_selection()
+        try:
+            select = int(input("Please select an option"))
+            menu_selection(select)
+        except ValueError:
+            print("Invalid selection. Please try again.")
